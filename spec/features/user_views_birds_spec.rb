d161ba0d0cce791cd91d5c688d8067cd3bc1a27a
afeinber <alexandernfeinberg@gmail.com>
2014-08-21 23:07:33 -0400

    updates tests to allow for searching by both scientific and common names

--- spec/features/user_views_birds_spec.rb
+++ spec/features/user_views_birds_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
-feature 'user views birds', js: true do
+feature 'user finds birds', js: true do
+  background do
+    @birds = create_list(:bird, 30)
+  end
 
-  it 'and searches for a bird' do
-    birds = create_list(:bird, 30)
-    last_bird_name = birds.last.common_name.delete(birds.last.common_name[0])
-    puts Bird.last.common_name
+  scenario 'and searches by common name' do
+
+    last_bird_name = @birds.last.common_name.delete(@birds.last.common_name[0])
 
     visit '/'
     click_link 'Find a bird'
+    click_link 'by common name'
 
     fill_in 'Find a bird', with: last_bird_name
 
-    expect(page).to have_content(birds.last.common_name)
+    expect(page).to have_content(@birds.last.common_name)
+
+  end
+
+  scenario 'and searches by scientific name' do
+
+    last_bird_name = @birds.last.scientific_name.delete(@birds.last.scientific_name[0])
+
+    visit '/'
+    click_link 'Find a bird'
+    click_link 'by scientific name'
+
+    fill_in 'Find a bird', with: last_bird_name
+
+    expect(page).to have_content(@birds.last.common_name)
 
   end
 end
