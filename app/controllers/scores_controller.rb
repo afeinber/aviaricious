17c212eaeba1cb2a6442388f84485bf112415d65
afeinber <alexandernfeinberg@gmail.com>
2014-08-29 11:23:50 -0400

    working on bug where user not signed in

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,12 +3,15 @@ class ScoresController < ApplicationController
   respond_to :json
 
   def create
+    binding.pry
     score = current_user.scores.create(score_params)
     respond_with score
   end
 
   def index
+    binding.pry
     scores = current_user.scores
+    binding.pry
     respond_with scores
   end
 
